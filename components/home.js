b86bac6caefe84bfd48daed495d84e7869fefc31
giulia-lir <giulia.libe3@gmail.com>
2023-11-15 15:48:38 +0200

    project migration

---
+++ components/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <View>
+            <Text style={styles.fontStyle}>This is the Home page</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    fontStyle: {
+      fontFamily: 'PlaypenSans',
+      fontSize: 20,
+      color: 'black'
+    }
+  });
