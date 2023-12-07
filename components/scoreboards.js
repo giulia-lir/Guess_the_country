7c797a5c8846dc249213d653b87c74066048b883
giulia-lir <giulia.libe3@gmail.com>
2023-12-07 22:50:55 +0200

    Firebase addition

--- components/scoreboards.js
+++ components/scoreboards.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { useFocusEffect } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { initializeApp } from "firebase/app";
+import { getDatabase, push, ref, onValue } from 'firebase/database';
 
+// SQLite local DB
 const db = SQLite.openDatabase('countriesdb.db');
+
+// Firebase DB connection
+const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_key,
+    authDomain: process.env.EXPO_PUBLIC_domain,
+    databaseURL: process.env.EXPO_PUBLIC_dburl,
+    projectId: process.env.EXPO_PUBLIC_projectId,
+    storageBucket: process.env.EXPO_PUBLIC_storage_bucket,
+    messagingSenderId: process.env.EXPO_PUBLIC_senderId,
+    appId: process.env.EXPO_PUBLIC_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firedb = getDatabase(app);
 
 export default function Scoreboards({ navigation }) {
     const [scoreList, setScoreList] = useState([]);
+    //const [highestScore, setHighestScore] = useState(0);
+    //const [leaderBoard, setLeaderBoard] = useState([]);
 
     const getScoreList = () => {
 
@@ -17,6 +37,7 @@ export default function Scoreboards({ navigation }) {
                 helper = [...results].sort((a, b) => b.endless_score - a.endless_score)
                 //console.log(helper)
                 setScoreList([...results].sort((a, b) => b.endless_score - a.endless_score))
+                //setHighestScore(scoreList[0])
             }
             ), (_, error) => console.error('Error fetching scores:', error)
         });
@@ -28,6 +49,10 @@ export default function Scoreboards({ navigation }) {
                 tx.executeSql('delete from endless_scores where id = ?;', [id]);
             }, (_, error) => console.error('Error deleting score:', error), getScoreList
         )
+    }
+
+    const saveScore = () => {
+        push(ref(firedb, 'leaderBoard/'), scoreList[0].endless_score);
     }
 
     useFocusEffect(
@@ -43,6 +68,7 @@ export default function Scoreboards({ navigation }) {
             end={{ x: 1, y: 0 }}
             style={styles.viewStyle}
         >
+            <Button title= "Save to Leaderboard" onPress={saveScore}/>
             <Text style={styles.titleStyle}>Your best scores</Text>
             {/* <Button title="Show List" onPress={getScoreList} /> */}
             {scoreList.length > 0 ? (
